65ebbf2db541e7253e9cf4e51fe984b43e19e1eb
Satisfraction <mathias.lappe@docc.techstarter.de>
2023-01-07 01:01:19 +0100

    Hausaufgabe - shouuld work

--- json.py
+++ json.py
@@ -1,6 +1,37 @@
 import json
 
-with open('tsjson/accounts.json') as user_file:
-  file_contents = user_file.read()
+user_file = open('tsjson/accounts.json')
+
   
-print(file_contents)
+print(user_file)
+
+
+data = json.load(user_file)
+
+
+while True:
+    eingabe = (input("Bitte gib alter oder Kontostand ein:\nq um abzubrechen.\n"))
+    if eingabe == "q":
+        print("Bis zum nächsten mal...")
+        break
+    elif eingabe == "alter":
+        for i in data:
+            if i["age"] > 25:
+                print(i["name"],i["age"])    
+    elif eingabe == "Kontostand":
+        money = []
+
+        for i in data: 
+            money.append(i["balance"])
+
+        highest = money[0]
+
+        for number in money:
+            if number > highest:
+                highest = number
+        print(highest)
+    else:
+        print("falsche Eingabe, versuche es erneut!")
+
+
+user_file.close()
